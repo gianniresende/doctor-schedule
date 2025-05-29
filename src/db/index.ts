0f7196dec59e12f22e495e3c533f99110dbb578a
gianniresende <gianniresende@gmail.com.br>
2025-05-29 10:21:06 -0300

    Implement dashboard authentication and clinic retrieval logic

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 
 import { drizzle } from 'drizzle-orm/node-postgres';
 
-const db = drizzle(process.env.DATABASE_URL!);
+import * as schema from './schema';
+
+const db = drizzle(process.env.DATABASE_URL!, { schema });
 
 export default db;
